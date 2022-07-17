0997f1cce44091344a4c81286f048197c2bf7736
KosukeNiwa <business.nknknk@gmail.com>
2022-07-17 17:14:30 +0900

    質問

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+
+  post "posts/create" => "posts#create"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 end
